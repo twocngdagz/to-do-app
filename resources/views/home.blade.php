7d8ccfa6e12fa05c875df53bc408e88edf032eeb
Mederic Roy Beldia <twocngdagz@yahoo.com>
2020-10-31 18:15:17 +0800

    init

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+    @extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <chart></chart>
+            <to-do></to-do>
+        </div>
+    </div>
+</div>
+@endsection
